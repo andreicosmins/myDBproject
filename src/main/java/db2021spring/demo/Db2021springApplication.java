96e447059e0735509f20e8efcb910eb73251a9cf
Andrei Cosmin Stan <andreicosmin.stan@gmail.com>
2021-04-30 17:37:17 +0200

    Updated Project

--- src/main/java/db2021spring/demo/Db2021springApplication.java
+++ src/main/java/db2021spring/demo/Db2021springApplication.java
@@ -7,28 +7,87 @@ import java.util.Scanner;
 
 @SpringBootApplication
 public class Db2021springApplication {
-    public static Scanner input= new Scanner(System.in);
+    public static Scanner scanner= new Scanner(System.in);
     enum Operation {
         CREATE,
         READ,
         UPDATE,
         DELETE
     }
-    public static void InputReader(){
-        System.out.println("Enter operation:" + "C to create " + "R to list " +"U to update " + "D to delete" +" Q to quit" );
-        String operation = input.nextLine();
-        switch (operation){
-            case "C":
-                System.out.println("Create user" );
+    enum Type {
+        Customer,
+        Order
+    }
+    public static String InputData(Operation operation, Type type){
+        switch (type) {
+            case Customer:
+                switch (operation){
+                    case CREATE:
+                        System.out.println(" Enter customer name");
+                        break;
+                    case DELETE:
+                        System.out.println("Enter customer ID");
+                        break;
+                    case UPDATE:
+                        System.out.println("Enter customer ID, to update name (ID,Name)");
+                        break;
+                }
+                break;
+            case Order:
+                switch (operation) {
+                    case CREATE:
+                        System.out.println("Enter customer ID and order (ID,Order)");
+                        break;
+                }
 
                 break;
-            case "R":
+        }
+        if (!operation.equals(Operation.READ)){
+            String input =scanner.nextLine();
+            return input;
+        }
+
+        return "";
+
+    }
+    public static Type InputType(){
+        System.out.println("Choose CU Customer /n OR Order Q Quit ");
+        String input =scanner.nextLine();
+        Type type = null;
+        switch (input) {
+            case "CU": type=Type.Customer;
+            System.out.println("Customer");
+            break;
+            case "OR": type=Type.Order;
+                System.out.println("Order");
+            break;
+            case "Q":
+                System.out.println("Quit");
+                System.exit(0);
+            default:
+                System.out.println("Incorrect input");
+
+        }
+        return type;
+
+    }
+    public static void InputReader(){
+        Type type = InputType();
+        System.out.println("Enter operation:" + "C to create " + "R to list " +"U to update " + "D to delete" +" Q to quit" );
+        String input = scanner.nextLine();
+        Operation operation = null;
+        switch (input){
+            case "C":
+                operation=Operation.CREATE;
+                System.out.println("Create user" );
+                break;
+            case "R": operation=Operation.READ;
                 System.out.println("Read list");
                 break;
-            case "U":
+            case "U": operation=Operation.UPDATE;
                 System.out.println("Update name");
                 break;
-            case "D":
+            case "D": operation=Operation.DELETE;
                 System.out.println("Delete name");
                 break;
             case "Q":
@@ -38,16 +97,21 @@ public class Db2021springApplication {
                 System.out.println("Incorrect operation");
 
         }
+        if (operation!=null&&type!=null) {
+            System.out.println(operation.toString() + " " + type.toString());
+            String data=InputData(operation,type);
+            new MyDB(operation,type,data);
+
+        }
 
         InputReader(); // recursion
-
     }
     public static void main(String[] args) {
 
         InputReader();
 
         // SpringApplication.run(Db2021springApplication.class, args);
-        // new MyDB();
+
     }
 
 
--- src/main/java/db2021spring/demo/MyDB.java
+++ src/main/java/db2021spring/demo/MyDB.java
@@ -4,14 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MyDB {
-    public MyDB () {
-
-
+    public MyDB (Db2021springApplication.Operation Operation, Db2021springApplication.Type Type,String data) {
        String username="root";
-       String password="87mosCraciun19";
+       String password="870MCnts#";
        String database="MyDB";
        String url="jdbc:mysql://localhost:3306/";
         try {
@@ -19,28 +18,84 @@ public class MyDB {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-         Scanner inp= new Scanner(System.in);
-        System.out.println("Enter username");
-        String user_name = inp.nextLine();
+
+        //System.out.println(Operation.toString() + " " + Type.toString());
 
 
        try (Connection con= DriverManager.getConnection(url + database, username, password)){
            if(!con.isClosed()){
                // System.out.println("Ok Connecting");
+               String sql=null;
+               PreparedStatement ps=null;
 
-               String sql= "INSERT INTO Customers values (null,?)";
-               PreparedStatement ps=con.prepareStatement(sql);
-               ps.setString(1,user_name); // Tells what number of ? we refer to
-               ps.executeUpdate();
-               // Getting the rows
-               sql="SELECT * FROM Customers";
-               ps= con.prepareStatement(sql);
-               ResultSet resultSet= ps.executeQuery();
+               switch (Type){
+                   case Customer:
+                       switch (Operation){
+                           case CREATE:  sql= "INSERT INTO Customers values (null,?)";
+                                ps=con.prepareStatement(sql);
+                               ps.setString(1,data); // Tells what number of ? we refer to
+                               ps.executeUpdate();
+                               break;
+                           case READ:
+                                sql="SELECT * FROM Customers";
+                                ps=con.prepareStatement(sql);
+                               ResultSet resultSet= ps.executeQuery();
 
-               while (resultSet.next()){
+                               while (resultSet.next()){
 
-                   String FirstName=resultSet.getString("FirstName");
-                   System.out.println("Name:" + FirstName);
+                                   String FirstName=resultSet.getString("FirstName");
+                                   Integer CustomerID=resultSet.getInt("CustomerID");
+
+                                   System.out.println(" ID " + CustomerID + " Name:" + FirstName);
+                               }
+                               break;
+                           case DELETE: sql= "DELETE FROM Customers WHERE (`CustomerID` = ?);";
+                                        ps=con.prepareStatement(sql);
+                                        ps.setString(1,data);
+                                        ps.executeUpdate();
+                                        break;
+                           case UPDATE:    sql= "UPDATE Customers SET `FirstName` = ? WHERE (`CustomerID` = ?);";
+                               ps=con.prepareStatement(sql);
+                               String FirstName;
+                               String ID;
+                               String[] List;
+                               List=data.split(",");
+                               ID=List[0];
+                               FirstName=List[1];
+                               ps.setString(1,FirstName);
+                               ps.setString(2,ID);
+                               ps.executeUpdate();
+                               break;
+
+                       } break;
+                   case Order:
+                       switch (Operation){
+                           case CREATE: sql= "INSERT INTO Orders (`customerID`, `Order`) VALUES (? ,? );";
+                           String Order;
+                           String ID;
+                           String [] List;
+                           List=data.split(",");
+                           ID=List[0];
+                           Order=List[1];
+                           ps=con.prepareStatement(sql);
+                           ps.setString(1,ID);
+                           ps.setString(2,Order);
+                           ps.executeUpdate();
+                               break;
+                           case READ: sql="SELECT * FROM Orders";
+                               ps=con.prepareStatement(sql);
+                               ResultSet resultSet= ps.executeQuery();
+
+                               while (resultSet.next()){
+
+                                   String OrderID=resultSet.getString("OrderID");
+                                   Integer CustomerID=resultSet.getInt("CustomerID");
+                                   String order= resultSet.getNString("Order");
+
+                                   System.out.println(" Order ID " + OrderID + " CustomerID:" + CustomerID + " Order " + order);
+                               }
+                               break;
+                       } break;
                }
            }else {
                System.out.println(":-( no connection");
